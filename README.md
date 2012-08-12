9b20244d0f2c99c881b3c2d88f787b5b1127f3ad
James Otten <james_otten@lavabit.com>
2012-08-11 23:35:59 -0500

    Modified pythagoreanTriplet.d for euler 39

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Code written while learning D.
 * permutation.d - Iteratively generates permutations of a given array like C++'s STL next_permutation() (Euler 24 and 43)
 * morseCode.d - Translates console input to Morse code.
 * sumOfPrimes.d - Calculates the sum of primes below a given number, uses the sieve of Eratosthenes.
-* pythagoreanTriplet.d - Finds Pythagorean triplets with a user defined sum.
+* pythagoreanTriplet.d - Finds Pythagorean triplets with a user defined sum. Modified for Euler 39
 * greatestProduct.d - Find the greatest product of N consecutive digits of the user's number.
 * comments.d - Removes C style comments from stdin.
 * weighted graph/
--- pythagoreanTriplet.d
+++ pythagoreanTriplet.d
@@ -1,17 +1,38 @@
 /*
  * Finds Pythagorean triplets with a user defined sum
+ * Edited for euler 39
  * Copyright 2012 James Otten <james_otten@lavabit.com>
 */
 
 import std.stdio;
 
-void main() {
-	int input;
-	write("Enter the sum of your desired Pythagorean triplet(s): ");
-	readf("%d", &input);
-	
+int numberPythagoreanTriplets(int p) {
+	int ret;
+	for(int i = 1; i < p - 2; i++) //Each side must be at least 1
+		for(int j = i; j < p - 2; j++)
+			if(i^^2 + j^^2 == (p - i - j)^^2)
+				ret++;
+	return ret;
+}
+
+//This is what this program used to do
+void printPythagoreanTriplets(int input) {
 	for(int i = 1; i < input - 2; i++) //Each side must be at least 1
 		for(int j = i; j < input - 2; j++)
 			if(i^^2 + j^^2 == (input - i - j)^^2)
 				writefln("a=%d, b=%d, c=%d", i, j, input - i - j);
+}
+
+unittest {
+	assert(numberPythagoreanTriplets(120) == 3, "numberPythagoreanTriplets() failed euler example");
+}
+
+void main() {
+	int max = 3, maxIndex = 3, temp;
+	for(int i = maxIndex; i <= 1000; i++)
+		if((temp = numberPythagoreanTriplets(i)) > max) {
+			max = temp;
+			maxIndex = i;
+		}
+	writeln("39: ", maxIndex);
 }
